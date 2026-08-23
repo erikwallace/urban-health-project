9c14fa76359e497ff2ee630b7d27e5b3a36baece
Erik Wallace <erikdwallace@gmail.com>
2026-08-23 12:13:21 -0400

    Performance updates
    Reduce render blocking items that were slowing down page load. Remove unneeded jQuery, update Google Fonts and stylesheet links to be more performant.

--- _site/js/debug.js
+++ _site/js/debug.js
@@ -2,31 +2,35 @@
 // Debugger that reports the viewport size.
 // -----------
 function showViewportSize(display) {
-  
+
   if(display) {
-    
+
     var height = window.innerHeight;
     var width = window.innerWidth;
-    
-    jQuery('body').prepend('<div id="jq-viewportsize" style="z-index:9999;position:fixed;bottom:0px;left:0px;color:#fff;background:rgba(0,0,0,0.85);color:goldenrod;font-size:13px;letter-spacing:1px;padding:12px;">'+width+' x '+height+'</div>');
-    
-    jQuery(window).resize(function() {
+
+    document.body.insertAdjacentHTML('afterbegin', '<div id="jq-viewportsize" style="z-index:9999;position:fixed;inset-block-end:0px;inset-inline-start:0px;color:#fff;background:rgba(0,0,0,0.85);color:goldenrod;font-size:13px;letter-spacing:1px;padding:12px;">'+width+' x '+height+'</div>');
+
+    window.addEventListener('resize', function() {
             height = window.innerHeight;
             width = window.innerWidth;
-            jQuery('#jq-viewportsize').html(''+width+' x '+height);
+            document.getElementById('jq-viewportsize').innerHTML = ''+width+' x '+height;
     });
   }
 
 }
 
-$(document).click(function(e) {
-  $('#jq-viewportsize').hide();
+document.addEventListener('click', function(e) {
+  var el = document.getElementById('jq-viewportsize');
+  if (el) el.style.display = 'none';
 });
 
-$('#jq-viewportsize').click(function(e) {
-  e.stopPropagation();
-});
+var viewportSizeEl = document.getElementById('jq-viewportsize');
+if (viewportSizeEl) {
+  viewportSizeEl.addEventListener('click', function(e) {
+    e.stopPropagation();
+  });
+}
 
-$(document).ready(function(){
+document.addEventListener('DOMContentLoaded', function(){
    showViewportSize(true);
 });
